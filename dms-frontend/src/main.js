f2c6bd5ddf333be18b6a0a732a3b6a5a00bdd5ba
MDietermann <marvin.dietermann@wagner-ecommerce.group>
2024-11-29 10:47:21 +0100

    new Frontend

--- dms-frontend/src/main.js
+++ dms-frontend/src/main.js
@@ -1,46 +1,5 @@
-//import { createApp } from 'vue'
-//import App from './App.vue'
-//import router from './router'
-//
-//createApp(App).use(router).mount('#app')
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-import { app, BrowserWindow } from 'electron';
-import path from 'path';
-import url from 'url';
-
-
-let mainWindow = null;
-
-function createMainWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'dms-frontend/dist/index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
-}
-
-console.log(app);
-
-app.on('ready', createMainWindow);
-
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', function () {
-    if (mainWindow === null) {
-        createMainWindow();
-    }
-})
+createApp(App).mount('#app')
